6d85af40b14f6d9e0286646b2d087e0af4c3c9f1
IGP <pyltsov@gmail.com>
2020-12-19 16:52:23 +0100

    Initial Commit

---
+++ Mark-I/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Mark-I
+//
+//  Created by ИГОРЬ on 19/12/2020.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
